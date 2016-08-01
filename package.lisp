48bc0210e881b86401ad88c867a2d17ba6ee7892
Quinn Evans <qgevans@gmail.com>
2016-08-01 13:04:28 -0600

    Update package (new nickname, exports)

--- package.lisp
+++ package.lisp
@@ -1,5 +1,7 @@
 (defpackage ratatoskr
+  (:nicknames #:rat)
   (:use #:common-lisp #:usocket #:solipsism
 	#:alexandria #:bordeaux-threads)
-  (:export #:+max-params+
-	   #:*params-per-message*))
+  (:export #:+max-params+ #:*params-per-message*
+	   #:*command-map* #:command-mapping
+	   #:make-prefix #:copy-prefix #:prefix-nick #:prefix-user #:prefix-host #:prefix-p))

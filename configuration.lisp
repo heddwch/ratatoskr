a4a1858ac6649a89ffb50511b6c8b34bc74250e7
Quinn Evans <qgevans@gmail.com>
2016-08-01 22:22:06 -0600

    Enlarge default *targmax*, configure constructors

--- configuration.lisp
+++ configuration.lisp
@@ -7,13 +7,14 @@
      (cmd-names . 1)
      (cmd-privmsg . 4)
      (cmd-notice . 4)
-     (cmd-whois . 4)))
+     (cmd-whois . 4)
+     (cmd-userhost . 5)))
 
 (defparameter *targmaxes*
   (alist-hash-table +default-targmaxes+ :test 'eq))
 
 (defconstant +max-params+ 10)
-(defvar *targmax* 4)
+(defvar *targmax* 5)
 (proclaim
  `(type (or (integer 1 ,+max-params+) null) *operands-per-message*))
 
